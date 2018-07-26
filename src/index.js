f75fab0658cc9e28b21fe69fd3a71a3f3ba84e56
Michael Esch <eschmichael@yandex.com>
2018-07-25 21:19:34 -0400

    Integrate contest test data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import App from './components/App';
+import data from './testData.json';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App contests={data.contests} />, document.getElementById('root'));
