d456dfc0f90dea749fa07f82b9e59baca652d915
Michael Esch <eschmichael@yandex.com>
2018-07-26 14:40:25 -0400

    Refactor contest data to App state

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
+import data from '../testData.json';
 
 class App extends React.Component {
   state = {
     pageHeader: 'Naming Contests',
+    contests: [],
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.setState({
+      contests: data.contests,
+    });
+  }
 
   componentWillUnmount() {}
 
   render() {
-    const { contests } = this.props;
-    const { pageHeader } = this.state;
+    const { pageHeader, contests } = this.state;
     return (
       <div className="App">
         <Header message={pageHeader} />
         <div>
-          {contests.map(contest => <ContestPreview contest={{ ...contest }} />)}
+          {contests.map(contest => <ContestPreview key={contest.id} contest={{ ...contest }} />)}
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import data from './testData.json';
 
-ReactDOM.render(<App contests={data.contests} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
