1d7f9d9cbe1c13eb02d00328e94d5fd33f205580
Michael Esch <eschmichael@yandex.com>
2018-07-25 22:05:55 -0400

    Add styles

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
 import config from './config';
 import apiRouter from './api';
 
+
 const server = express();
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public'),
+}));
 server.set('view engine', 'ejs');
 
 server.get('/', (req, res) => {
