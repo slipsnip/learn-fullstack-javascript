c436ea084e5de99a78ecb945e7aa9300e95b0c4d
Michael Esch <eschmichael@yandex.com>
2018-07-25 21:20:13 -0400

    Add contest previews

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
   state = {
     pageHeader: 'Naming Contests',
   };
 
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
   render() {
+    const { contests } = this.props;
+    const { pageHeader } = this.state;
     return (
       <div className="App">
-        <Header message={ this.state.pageHeader } />
+        <Header message={pageHeader} />
         <div>
-          ...
+          {contests.map(contest => <ContestPreview contest={{ ...contest }} />)}
         </div>
       </div>
     );
