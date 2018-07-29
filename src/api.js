3a384216339a53a0e33e8e2100df66da0ceeccd1
Michael Esch <eschmichael@yandex.com>
2018-07-29 17:58:22 -0400

    Add fetchContestList

--- src/api.js
+++ src/api.js
@@ -4,3 +4,8 @@ export const fetchContest = contestId => (
   axios.get(`/api/contest/${contestId}`)
     .then(res => res.data)
 );
+
+export const fetchContestList = () => (
+  axios.get('/api/contests')
+    .then(res => res.data.contests)
+);
