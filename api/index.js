e3122386737e0fb09502ed7eabccc2a55a161900
Michael Esch <eschmichael@yandex.com>
2018-07-29 19:48:37 -0400

    Get data from database, remove fake data

--- api/index.js
+++ api/index.js
@@ -1,23 +1,41 @@
 import express from 'express';
-import data from '../src/testData.json';
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+import config from '../config';
+
+let mdb = null;
+MongoClient.connect(`${config.mongodbUri}/${config.mongodbName}`,
+  { useNewUrlParser: true },
+  (err, client) => {
+    assert.equal(null, err);
+    mdb = client.db(`${config.mongodbName}`);
+  });
 
 const router = express.Router();
 
-const contests = data.contests.reduce((obj, contest) => {
-  const newObj = { ...obj };
-  newObj[contest.id] = contest;
-  return newObj;
-}, {});
-
 router.get('/contests', (req, res) => {
-  res.send({ contests });
+  const contests = {};
+  mdb.collection('contests').find({})
+    .project({
+      id: 1,
+      categoryName: 1,
+      contestName: 1,
+    })
+    .each((err, contest) => {
+      assert.equal(null, err);
+      if (!contest) {
+        res.send({ contests });
+        return;
+      }
+      contests[contest.id] = contest;
+    });
 });
 
 router.get('/contest/:contestId', (req, res) => {
-  const contest = contests[req.params.contestId];
-  contest.description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, tenetur.';
-
-  res.send(contest);
+  mdb.collection('contests')
+    .findOne({ id: Number(req.params.contestId) })
+    .then(contest => res.send(contest))
+    .catch(console.error);
 });
 
 export default router;
