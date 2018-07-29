9503993a615049e57c0ffe8a6025d05bec26e432
Michael Esch <eschmichael@yandex.com>
2018-07-29 19:46:14 -0400

    Add mongodb config

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ export const nodeEnv = env.NODE_ENV || 'development';
 export const logStars = message => console.info(`*********${message}*********`);
 
 export default {
+  mongodbUri: 'mongodb://localhost:8888',
+  mongodbName: 'test',
   port: env.PORT || 8080,
   host: env.HOST || '0.0.0.0',
   get serverUrl() {
