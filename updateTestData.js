aa3bf3ee272fe4dda845560f9fb13c7edd944b13
Michael Esch <eschmichael@yandex.com>
2018-07-30 14:29:01 -0400

    Add updateTestData script

---
+++ updateTestData.js
@@ -0,0 +1,41 @@
+/* eslint no-underscore-dangle: 0 */
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+import config from './config';
+
+MongoClient.connect(
+  `${config.mongodbUri}/${config.mongodbName}`,
+  { useNewUrlParser: true },
+  (err, client) => {
+    assert.equal(null, err);
+    const db = client(`${config.mongodbName}`);
+    let contestCount = 0;
+    db.collection('contests')
+      .find({})
+      .each((error, contest) => {
+        assert.equal(null, error);
+        if (!contest) {
+          return;
+        }
+
+        contestCount += 1;
+        db.collection('names')
+          .find({ id: { $in: contest.nameIds } })
+          .project({ _id: 1 })
+          .toArray()
+          .then((_ids) => {
+            const newIds = _ids.map(o => o._id);
+            db.collection('contests')
+              .updateOne({ id: contest.id }, { $set: { nameIds: newIds } })
+              .then(() => {
+                console.info('Updated', contest._id);
+                contestCount -= 1;
+                if (contestCount === 0) {
+                  client.close();
+                }
+              });
+          })
+          .catch(console.error);
+      });
+  },
+);
