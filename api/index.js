a9430d8437ecaea6d425b5dc7c60be71112c8099
Michael Esch <eschmichael@yandex.com>
2018-07-30 22:08:08 -0400

    Add post handler for submited names

--- api/index.js
+++ api/index.js
@@ -4,18 +4,45 @@ import assert from 'assert';
 import config from '../config';
 
 let mdb = null;
-MongoClient.connect(`${config.mongodbUri}/${config.mongodbName}`,
+MongoClient.connect(
+  `${config.mongodbUri}/${config.mongodbName}`,
   { useNewUrlParser: true },
   (err, client) => {
     assert.equal(null, err);
     mdb = client.db(`${config.mongodbName}`);
-  });
+  },
+);
+
+// High order function: handle bad requests
+const catchBadRequest = promise => (req, res) => (
+  promise.then(data => data)
+    .catch((err) => {
+      console.error(err);
+      res.status(404).send('Bad Request!');
+    })
+);
+
+// Find and modify contest, return modified
+async function modifyContest(db, name, contestId) {
+  const result = await db.collection('names').insertOne({ name });
+  const document = await db
+    .collection('contests')
+    .findAndModify(
+      { _id: contestId },
+      [],
+      { $push: { nameIds: result.insertedId } },
+      { new: true },
+    );
+  return [document, result.insertedId];
+}
 
 const router = express.Router();
 
 router.get('/contests', (req, res) => {
   const contests = {};
-  mdb.collection('contests').find({})
+  mdb
+    .collection('contests')
+    .find({})
     .project({
       categoryName: 1,
       contestName: 1,
@@ -34,7 +61,9 @@ router.get('/names/:nameIds', (req, res) => {
   const names = {};
   const nameIds = req.params.nameIds.split(',').map(ObjectID);
 
-  mdb.collection('names').find({ _id: { $in: nameIds } })
+  mdb
+    .collection('names')
+    .find({ _id: { $in: nameIds } })
     .each((err, name) => {
       assert.equal(null, err);
       if (!name) {
@@ -46,10 +75,21 @@ router.get('/names/:nameIds', (req, res) => {
 });
 
 router.get('/contest/:contestId', (req, res) => {
-  mdb.collection('contests')
+  catchBadRequest(mdb
+    .collection('contests')
     .findOne({ _id: ObjectID(req.params.contestId) })
-    .then(contest => res.send(contest))
-    .catch(console.error);
+    .then(contest => res.send(contest)));
+});
+
+router.post('/names', (req, res) => {
+  const { newName: name } = req.body;
+  const contestId = ObjectID(req.body.contestId);
+
+  catchBadRequest(modifyContest(mdb, name, contestId)
+    .then((doc, insertedId) => res.send({
+      updatedContest: doc.value,
+      newName: { _id: insertedId, name },
+    })));
 });
 
 export default router;
